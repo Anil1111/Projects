1a344b5e9c12e54c1a7eda3d2087ba7cb416c045
DMazak <drmazak@gmail.com>
2014-11-05 13:32:51 -0500

    Retrieves time and displays it in a message box

--- Web/AtomicClock/Web.java
+++ Web/AtomicClock/Web.java
@@ -5,11 +5,24 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import javax.swing.*;
 public class Web
 {
-public static void main(String[] args)
-{
-Document doc = Jsoup.connect("http://time.is/").get();
-
-}// end main
+   public static void main(String[] args)
+   {
+      try
+      {
+      Document doc = Jsoup.connect("http://time.is/").get();
+      String title = doc.title();
+      
+      Element time = doc.select("#twd").first();
+      // <div id="twd">01:19:25<span id="ampm" style="font-size:32px;line-height:32px">pm</span></div>
+      JOptionPane.showMessageDialog(null, time.text());
+      }
+      catch (IOException ex)
+      {
+      JOptionPane.showMessageDialog(null, ex.toString());
+      }
+   }// end main
 }// end class
