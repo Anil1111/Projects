5b0e435ba10b4a552405bb9162a1a9328212b5c6
DMazak <drmazak@gmail.com>
2014-11-05 12:13:42 -0500

    Setting up jsoup

--- Web/AtomicClock/Web.java
+++ Web/AtomicClock/Web.java
@@ -1,6 +1,11 @@
 /* web.java */
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-public static class Web
+public class Web
 {
 public static void main(String[] args)
 {
